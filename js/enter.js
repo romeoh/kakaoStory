284e427bed89a576b652669629061a195833057a
romeoh78@gmail.com <romeoh78@baeg-ui-MacBook-Pro.local>
2013-05-03 21:05:29 +0900

    update

--- js/enter.js
+++ js/enter.js
@@ -1,4 +1,9 @@
-var  userName
+var ua = navigator.userAgent
+	,os = (/iphone|ipad|ipod/gi).test(ua) ? "ios" : 
+		(/android/gi).test(ua) ? "android" :
+		(/mac/gi).test(ua) ? "macOS" : 
+		(/windows/gi).test(ua) ? "Windows" : "other"
+	,userName
 	,boy = document.getElementById('boy')
 	,girl = document.getElementById('girl')
 	,boySelect = document.querySelector('#boyBox a')
@@ -7,7 +12,16 @@ var  userName
 	,btnKakao = document.querySelector('#btnKakao')
 	,dataMale, dataFemale
 
-window.addEventListener("load", initPage, false);
+if (os == 'ios' || os == 'android') {
+	//init();
+} else {
+	var  adTop = document.querySelector('#adTop')
+		,adBottom = document.querySelector('#adBottom')
+	document.querySelector('body').removeChild(adTop)
+	document.querySelector('body').removeChild(adBottom)
+}
+
+window.addEventListener("DOMContentLoaded", initPage, false);
 function initPage(){
 	btnStory.addEventListener('click', executeKakaoStoryLink, false);
 	btnKakao.addEventListener('click', executeURLLink, false);
