f79a1129633b4649586456d9900c11c9d251a685
Brian Paek <romeoh78@gmail.com>
2013-05-13 18:35:13 +0900

    update

--- js/born.js
+++ js/born.js
@@ -30,23 +30,24 @@ function executeKakaoStoryLink(){
 	var  sexType
 		,userName = document.querySelector('#userName').value
 		,postMsg = ''
-		,dataCharactorRan = Math.floor(Math.random() * dataCharactor.length)
+		,dataRan = Math.floor(Math.random() * data.length)
 	
 	if (userName == '') {
 		alert('이름을 입력해 주세요.');
 		return false;
 	}
 	postMsg += '[내가 세상에 태어난 이유]\n\n';
-	postMsg += userName + '님이 세상에 태어난 이유는 입니다.\n';
-	postMsg += 'http://goo.gl/HaHyR';
+	postMsg += userName + '님이 세상에 태어난 이유는 \n';
+	postMsg += data[dataRan]['result'] + '\n';
+	postMsg += 'http://goo.gl/8ns8E\n';
 
 	urlMsg = {
 		title: '내가 세상에 태어난 이유',
-		desc: '엄마가 시켜서 입니다.',
-		imageurl: ['http://romeoh.github.io/kakaoStory/img/' ],
+		desc: data[dataRan]['result'],
+		imageurl: ['http://romeoh.github.io/kakaoStory/img/cake.jpg' ],
 		type:'article'
 	}
-console.log(postMsg)
+console.log(urlMsg)
 	kakao.link("story").send({   
         post : postMsg,
         appid : 'funnyApp',
@@ -59,19 +60,43 @@ console.log(postMsg)
 // 카톡
 function executeURLLink() {
 	kakao.link("talk").send({
-		msg: "나의 롤모델",
-		url: "http://goo.gl/HaHyR",
+		msg: "내가 세상에 태어난 이유",
+		url: "http://goo.gl/8ns8E",
 		appid: "funnyApp",
 		appver: "1.0",
-		appname: "나의 롤모델",
+		appname: "내가 세상에 태어난 이유",
 		type: "link"
 	});
 }
 
 
-dataCharactor = [
-	{'result': ''}
-	
+data = [
+	{'result': '돈을 많이 벌어서 잘먹고 잘살기 위해서 입니다.'},
+	{'result': '취직하기 위해서 입니다.'},
+	{'result': '영화배우가 되기 위해서 입니다.'},
+	{'result': '부모님이 시켜서 입니다.'},
+	{'result': '월급을 많이 받고 싶어서 입니다.'},
+	{'result': '결혼하고 싶어서 입니다.'},
+	{'result': '해외여행을 하고 싶어서 입니다.'},
+	{'result': '별 이유 없습니다.'},
+	{'result': '내 의지가 아니었습니다.'},
+	{'result': '생일빵을 받고 싶어서 입니다.'},
+	{'result': '애인이랑 무인도에 놀러가고 싶어서 입니다.'},
+	{'result': '우리나라를 전쟁에서 구하고 싶어서 입니다.'},
+	{'result': '부모님의 실수였습니다.'},
+	{'result': '연예인을 만나보고 싶어서 입니다.'},
+	{'result': '최고의 가수가 되고 싶어서 입니다.'},
+	{'result': '짜장면을 먹고 싶어서 입니다.'},
+	{'result': '우주 여행을 하고싶어서 입니다.'},
+	{'result': '군대에 가보고 싶어서 입니다.'},
+	{'result': '축구 선수가 되고 싶어서 입니다.'},
+	{'result': '모태솔로를 벗어나기 위해서 입니다.'},
+	{'result': '국가 기밀입니다.'},
+	{'result': '일진이 되고 싶어서 입니다.'},
+	{'result': '주차요원이 되고 싶어서 입니다.'},
+	{'result': '사랑받기 위해 태어났습니다.'},
+	{'result': '욕먹고 싶어서 태어났습니다.'},
+	{'result': '해적이 되고 싶어서 입니다.'}
 ]
 
 
