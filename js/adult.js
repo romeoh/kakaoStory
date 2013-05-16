092f4c4a402d19e826d1291611655d6e0c8d1019
Brian Paek <romeoh78@gmail.com>
2013-05-16 10:52:56 +0900

    update

--- js/adult.js
+++ js/adult.js
@@ -33,29 +33,37 @@ function executeKakaoStoryLink(){
 		,message	//
 		
 		,postMsg = ''
-		,urlMsg = {
-			title: ' 인사팀',
-			desc: '에서 ',
-			imageurl: ['http://romeoh.github.io/kakaoStory/imgCom/'],
-			type:'article'
-		}
+		,urlMsg
 	
-	setRandom(dataPresent)
-
-	postMsg += '성년의 날 받고싶은 선물 3가지\n';
-	postMsg += '[5월 20일 월요일]\n\n';
-	postMsg += '1. '+dataPresent[present0]['present']+'\n';
-	postMsg += '2. '+dataPresent[present1]['present']+'\n';
-	postMsg += '3. '+dataPresent[present2]['present']+'\n\n';
-	postMsg += 'http://goo.gl/QWPHM';
-	
-
 	if (age == '') {
 		alert('생년을 입력해 주세요.');
 		return false;
 	}
+	if (age > 1994) {
+		postMsg += '내가 성년이 되었을때 꼭 받고싶은 선물 3가지\n';
+		//console.log('받고싶을 선물')
+	} else if (age < 1993) {
+		postMsg += '성년의 날 못 받았던 선물 3가지\n';
+	} else {
+		postMsg += '성년의날 받고싶은 선물 3가지\n';
+	}
+
+	setRandom(dataPresent)
+
+	postMsg += '[5월 20일 월요일]\n\n';
+	postMsg += '1. '+dataPresent[present0]['present']+'\n';
+	postMsg += '2. '+dataPresent[present1]['present']+'\n';
+	postMsg += '3. '+dataPresent[present2]['present']+'\n\n';
+	postMsg += 'http://goo.gl/HZtpR';
 	
-	console.log(postMsg)
+	urlMsg = {
+			title: '성년의날 받고싶은 선물 3가지',
+			desc: '5월 20일',
+			imageurl: ['http://romeoh.github.io/kakaoStory/img/' + dataPresent[present0]['photo']],
+			type:'article'
+		}
+
+	console.log(postMsg, urlMsg)
 
 	kakao.link("story").send({   
 		post : postMsg,
@@ -91,11 +99,11 @@ function setRandom(data){
 // 카톡
 function executeURLLink() {
 	kakao.link("talk").send({
-		msg: "냉철한 대기업 인재 사냥꾼",
-		url: "http://goo.gl/QWPHM",
+		msg: "성년의 날 받고싶은 선물 3가지",
+		url: "http://goo.gl/HZtpR",
 		appid: "funnyApp",
 		appver: "1.0",
-		appname: "냉철한 대기업 인재 사냥꾼",
+		appname: "성년의 날",
 		type: "link"
 	});
 }
@@ -103,18 +111,35 @@ function executeURLLink() {
 
 
 dataPresent = [
-	{'present':'향수1', 'photo':'0'},
-	{'present':'향수2', 'photo':'0'},
-	{'present':'향수3', 'photo':'0'},
-	{'present':'향수4', 'photo':'0'},
-	{'present':'향수5', 'photo':'0'},
-	{'present':'향수6', 'photo':'0'},
-	{'present':'향수7', 'photo':'0'},
-	{'present':'향수8', 'photo':'0'},
-	{'present':'향수9', 'photo':'0'},
-	{'present':'향수10', 'photo':'0'},
-	{'present':'향수11', 'photo':'0'},
-	{'present':'향수12', 'photo':'0'},
+	{'photo':'present0.jpg', 'present':'향수'},
+	{'photo':'present1.jpg', 'present':'장미꽃'},
+	{'photo':'present2.jpg', 'present':'손편지'},
+	{'photo':'present3.jpg', 'present':'키스'},
+	{'photo':'present4.jpg', 'present':'손목시계'},
+	{'photo':'present5.jpg', 'present':'BMW'},
+	{'photo':'present6.jpg', 'present':'타워팰리스'},
+	{'photo':'present7.jpg', 'present':'돈봉투'},
+	{'photo':'present8.jpg', 'present':'산악자전거'},
+	{'photo':'present9.jpg', 'present':'운동화'},
+	{'photo':'present10.jpg', 'present':'상품권'},
+	{'photo':'present11.jpg', 'present':'카네이션'},
+	{'photo':'present12.jpg', 'present':'백과사전'},
+	{'photo':'present13.jpg', 'present':'과자세트'},
+	{'photo':'present14.jpg', 'present':'인삼세트'},
+	{'photo':'present15.jpg', 'present':'사과상자'},
+	{'photo':'present16.jpg', 'present':'커플속옷'},
+	{'photo':'present17.jpg', 'present':'카톡메세지'},
+	{'photo':'present18.jpg', 'present':'립스틱'},
+	{'photo':'present19.jpg', 'present':'화장품'},
+	{'photo':'present20.jpg', 'present':'면도기'},
+	{'photo':'present21.jpg', 'present':'화환'},
+	{'photo':'present22.jpg', 'present':'생선세트'},
+	{'photo':'present23.jpg', 'present':'국내산 농산물세트'},
+	{'photo':'present24.jpg', 'present':'한우세트'},
+	{'photo':'present25.jpg', 'present':'굴비세트'},
+	{'photo':'present26.jpg', 'present':'곶감세트'},
+	{'photo':'present27.jpg', 'present':'성형수술'},
+	{'photo':'present28.jpg', 'present':'갈비세트'}
 ]
 
 
