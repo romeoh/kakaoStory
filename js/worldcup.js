006f881ec818f65deb0b31cbadb81c8bf1e44b8a
Brian Paek <romeoh78@gmail.com>
2013-05-20 12:21:31 +0900

    update

--- js/worldcup.js
+++ js/worldcup.js
@@ -11,6 +11,7 @@ var ua = navigator.userAgent
 	,arr2
 	,arr1
 	,cuRound
+	,cuStage
 
 if (os == 'ios' || os == 'android') {
 	//init();
@@ -41,16 +42,29 @@ function init() {
 	arr2 = []
 	arr1 = []
 	cuRound = 0
+	cuStage = 32
 }
 
 
 function initStart() {
+	M('#userName').on('keyup', function(evt, mp){
+		//M('h1').text('연예인 이상형 '+mp.val()+'컵')
+	})
+
 	M('#btnBoy').click(function(){
+		if (M('#userName').val() == '') {
+			alert('이름을 입력하세요.');
+			return;
+		}
 		data = dataMale;
 		insert32(data, 32)
 		start(0)
 	})
 	M('#btnGirl').click(function(){
+		if (M('#userName').val() == '') {
+			alert('이름을 입력하세요.');
+			return;
+		}
 		data = dataFemale;
 		insert32(data, 32)
 		start(1)
@@ -61,25 +75,156 @@ function initStart() {
 function start(flag) {
 	M('#start').css('display', 'none');
 	M('#game').css('display', 'block');
-	M('#round').text('32강전');
-	M('#stage').text('(1/16)');
+	
+
+	M('.first').on('click', selPic)
+	M('.second').on('click', selPic)
 
 	fight(cuRound, 32);
 }
 
-function fight(round, final){
-	
-	console.log(data[ arr32[round] ])
-	console.log(data[ arr32[round+1] ])
-	console.log(round)
-	
-	M('#first').attr('src', '../img/' + data[ arr32[round] ]['photo'])
-	M('#firstPhoto').text(data[ arr32[round] ]['name'])
-	M('#second').attr('src', '../img/' + data[ arr32[round+1] ]['photo'])
-	M('#secondPhoto').text(data[ arr32[round+1] ]['name'])
+function selPic(evt, mp){
+	if (cuStage === 32) {
+		arr16.push(mp.data('idx'));
+		if (cuRound <= 14) {
+			cuRound++
+			fight(cuRound, 32);
+		} else {
+			cuStage = 16;
+			cuRound = 0
+			fight(cuRound, 16);
+		}
+	} else if (cuStage === 16) {
+		arr8.push(mp.data('idx'));
+		if (cuRound <= 6) {
+			cuRound++
+			fight(cuRound, 16);
+		} else {
+			cuStage = 8;
+			cuRound = 0
+			fight(cuRound, 8);
+		}
+	} else if (cuStage === 8) {
+		arr4.push(mp.data('idx'));
+		if (cuRound <= 2) {
+			cuRound++
+			fight(cuRound, 8);
+		} else {
+			cuStage = 4;
+			cuRound = 0
+			fight(cuRound, 4);
+		}
+	} else if (cuStage === 4) {
+		arr2.push(mp.data('idx'));
+		if (cuRound <= 0) {
+			cuRound++
+			fight(cuRound, 4);
+		} else {
+			cuStage = 2;
+			cuRound = 0
+			fight(cuRound, 2);
+		}
+	} else if (cuStage === 2) {
+		M('#game').css('display', 'none');
+		M('#result').css('display', 'block');
+		M('#winnerPhoto').attr('src', '../img/'+data[mp.data('idx')]['photo'])
+		M('#winnerName').text(data[mp.data('idx')]['name']);
+		console.log('winner', data[mp.data('idx')]['name'])
+	}
+}
 
-	if (cuRound <= final/2) {
-		cuRound++;
+function fight(round, final){
+	var  left = (Number(round)+Number(1)) * 2 - 2
+		,right = (Number(round)+Number(1)) * 2 - 1
+
+	if (left == -1) {
+		left = 0;
+	}
+	if (right == 0) {
+		right = 1;
+	}
+	//console.log(data[ arr32[left] ])
+	//console.log(data[ arr32[right] ])
+	//console.log(round)
+	
+	if (final === 32) {
+		M('#first').attr('src', '../img/' + data[ arr32[left] ]['photo'])
+		M('#firstPhoto')
+			.text(data[ arr32[left] ]['name'])
+			.parent()
+			.data('idx', arr32[left])
+		M('#second').attr('src', '../img/' + data[ arr32[right] ]['photo'])
+		M('#secondPhoto')
+			.text(data[ arr32[right] ]['name'])
+			.parent()
+			.data('idx', arr32[right])
+
+		M('#round').text('32강전');
+		M('#stage').text('('+(cuRound+1)+'/16)');
+	}
+
+	if (final === 16) {
+		M('#first').attr('src', '../img/' + data[ arr16[left] ]['photo'])
+		M('#firstPhoto')
+			.text(data[ arr16[left] ]['name'])
+			.parent()
+			.data('idx', arr16[left])
+		M('#second').attr('src', '../img/' + data[ arr16[right] ]['photo'])
+		M('#secondPhoto')
+			.text(data[ arr16[right] ]['name'])
+			.parent()
+			.data('idx', arr16[right])
+
+		M('#round').text('16강전');
+		M('#stage').text('('+(cuRound+1)+'/8)');
+	}
+
+	if (final === 8) {
+		M('#first').attr('src', '../img/' + data[ arr8[left] ]['photo'])
+		M('#firstPhoto')
+			.text(data[ arr8[left] ]['name'])
+			.parent()
+			.data('idx', arr8[left])
+		M('#second').attr('src', '../img/' + data[ arr8[right] ]['photo'])
+		M('#secondPhoto')
+			.text(data[ arr8[right] ]['name'])
+			.parent()
+			.data('idx', arr8[right])
+
+		M('#round').text('8강전');
+		M('#stage').text('('+(cuRound+1)+'/4)');
+	}
+
+	if (final === 4) {
+		M('#first').attr('src', '../img/' + data[ arr4[left] ]['photo'])
+		M('#firstPhoto')
+			.text(data[ arr4[left] ]['name'])
+			.parent()
+			.data('idx', arr4[left])
+		M('#second').attr('src', '../img/' + data[ arr4[right] ]['photo'])
+		M('#secondPhoto')
+			.text(data[ arr4[right] ]['name'])
+			.parent()
+			.data('idx', arr4[right])
+
+		M('#round').text('준결승');
+		M('#stage').text('('+(cuRound+1)+'/2)');
+	}
+
+	if (final === 2) {
+		M('#first').attr('src', '../img/' + data[ arr4[left] ]['photo'])
+		M('#firstPhoto')
+			.text(data[ arr4[left] ]['name'])
+			.parent()
+			.data('idx', arr4[left])
+		M('#second').attr('src', '../img/' + data[ arr4[right] ]['photo'])
+		M('#secondPhoto')
+			.text(data[ arr4[right] ]['name'])
+			.parent()
+			.data('idx', arr4[right])
+
+		M('#round').text('결승');
+		M('#stage').text('');
 	}
 }
 
@@ -122,20 +267,13 @@ function executeKakaoStoryLink(){
 	var  sexType
 		,userName = document.querySelector('#userName').value
 		,postMsg = ''
-		,dataRan = Math.floor(Math.random() * data.length)
 	
-	if (userName == '') {
-		alert('이름을 입력해 주세요.');
-		return false;
-	}
-	
-	postMsg += '[3년뒤 나의 외제차]\n';
-	postMsg += '3년뒤 ' + userName + '님의 애마는 \n';
-	postMsg += data[dataRan]['price'] + '짜리 ' + data[dataRan]['car'] + '입니다.\n\n';
-	postMsg += 'http://goo.gl/yuAkj\n';
+	postMsg += '[' + M('#userName')val() + '배 연예인 월드컵]\n';
+	postMsg += M('#userName')val() + '배 연예인 월드컵 영광의 최종 우승자는 고아라입니다.\n';
+	postMsg += '\n';
 
 	urlMsg = {
-		title: '3년뒤 나의 외제차',
+		title: '연예인 월드컵',
 		desc: data[dataRan]['car'],
 		imageurl: ['http://romeoh.github.io/kakaoStory/img/' + data[dataRan]['photo'] ],
 		type:'article'
@@ -145,7 +283,7 @@ console.log(postMsg, urlMsg)
         post : postMsg,
         appid : 'funnyApp',
 		appver : '1.0',
-		appname : '3년뒤 나의 외제차',
+		appname : '연예인 월드컵',
 		urlinfo : JSON.stringify(urlMsg)
     });
 }
@@ -153,11 +291,11 @@ console.log(postMsg, urlMsg)
 // 카톡
 function executeURLLink() {
 	kakao.link("talk").send({
-		msg: "3년뒤 나의 외제차",
+		msg: "연예인 월드컵",
 		url: "http://goo.gl/yuAkj",
 		appid: "funnyApp",
 		appver: "1.0",
-		appname: "3년뒤 나의 외제차",
+		appname: "연예인 월드컵",
 		type: "link"
 	});
 }
