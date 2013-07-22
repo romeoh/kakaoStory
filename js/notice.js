8b63bf18225c7c65aa644d3d5190e089de9c2748
romeoh <romeoh78@gmail.com>
2013-07-22 17:18:04 +0900

    update

--- js/notice.js
+++ js/notice.js
@@ -78,58 +78,6 @@ function executeURLLink() {
 	});
 }
 
-function setRandom(data){
-	var idx = Math.floor(Math.random() * data.length)
-	if (jogeun0 == undefined) {
-		jogeun0 = idx;
-	}
-	if (jogeun1 == undefined) {
-		if (jogeun0 == idx) {
-			setRandom(data)
-		} else {
-			jogeun1 = idx;
-		}
-	}
-	if (jogeun2 == undefined) {
-		if (jogeun0 == idx || jogeun1 == idx) {
-			setRandom(data)
-		} else {
-			jogeun2 = idx;
-			return jogeun2;
-		}
-	}
-	if (jogeun3 == undefined) {
-		if (jogeun0 == idx || jogeun1 == idx || jogeun2 == idx) {
-			setRandom(data)
-		} else {
-			jogeun3 = idx;
-			return jogeun3;
-		}
-	}
-}
-
-
-data = [
-	{'constinue':'키가 크고', 'last':'휀칠하게 키가 크지만', 'fault':'키가...Y.Y'},
-	{'constinue':'진정한 시대의 얼짱이고', 'last':'얼굴만은 자신있지만', 'fault':'얼굴이 죄송합니다.T.T'},
-	{'constinue':'돈이 많고', 'last':'가진 돈이 많지만', 'fault':'돈이 없어요.'},
-	{'constinue':'인기가 많고', 'last':'인기가 많지만', 'fault':'사람들한테 왕따 당해요.'},
-	{'constinue':'머리가 좋고', 'last':'머리가 엄청 좋지만', 'fault':'머리에 든게 없어요.'},
-	{'constinue':'유머가 있고', 'last':'유머러스 하지만', 'fault':'유머 감각이 없어요.'},
-	{'constinue':'싸움을 잘하고', 'last':'누구와 싸움에서 지지는 않지만', 'fault':'싸움을 못해요.'},
-	{'constinue':'노래를 잘하고', 'last':'노래를 정말 잘하지만', 'fault':'음치박치에요.'},
-	{'constinue':'매너 있고', 'last':'매너 짱이지만', 'fault':'매너가 똥이랍니다.'},
-	{'constinue':'배려심이 많고', 'last':'배려심이 아주 많지만', 'fault':'배려심이 없어요.'},
-	{'constinue':'일편단심 스타일이고', 'last':'한사람만 바라보는 일편단심 스타일 이지만', 'fault':'바람둥이에요.'},
-	{'constinue':'옷을 잘 입고', 'last':'돋보이는 패셔니스타지만', 'fault':'패션이 촌스러워요.'},
-	{'constinue':'몸매가 좋고', 'last':'몸매에 자신있지만', 'fault':'몸매가 나빠요..'},
-	{'constinue':'섹시 하고', 'last':'섹시가 철철 넘치지만', 'fault':'섹시하지 않아요.'},
-	{'constinue':'능력 있고', 'last':'능력 있지만', 'fault':'능력없는 백수랍니다.'},
-	{'constinue':'세련 되고', 'last':'온몸이 세련되어 보이지만', 'fault':'촌스러워요.'},
-	{'constinue':'영리해 보이고', 'last':'똑똑해 보이지만', 'fault':'띨 해보여요~'}
-]
-
-
 
 
 
