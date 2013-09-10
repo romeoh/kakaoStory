ace35fd9f27c340edf7239a3761989958a692e6e
romeoh <romeoh78@gmail.com>
2013-09-10 14:07:30 +0900

    update

--- js/meet.js
+++ js/meet.js
@@ -11,7 +11,9 @@ function action(_data) {
 		,bornDate = data.bornDate || null
 		,blood = data.blood || null
 		,post = ''
+		,twit = ''
 		,str = ''
+		,tstr = ''
 
 	data.title = '이성을 만나는 시기';
 	data.url = 'http://goo.gl/HoznT';
@@ -26,12 +28,18 @@ function action(_data) {
 	for (var i=0; i<53; i++) {
 		var value = getRandom(0, 99)
 		str += (firstYear + i) +'년('+(18+i)+'세) ' + getGraph(value) + ' '+value+'%\n'
+		tstr += (18+i)+'세 '+value+'%\n'
 	}
 
 	post += '[' + data.title + ']\n\n';
 	post += M('#userName').val() + '님이 이성을 만나는 시기입니다.\n\n';
 	post += str;
+
+	twit += '[' + data.title + ']\n\n';
+	twit += tstr;
+	
 	data.post = post;
+	data.twit = twit;
 	
 	data.desc = '이성을 만나는 시기';
 	data.img = 'http://romeoh.github.io/kakaoStory/img/relationLover.jpg';
