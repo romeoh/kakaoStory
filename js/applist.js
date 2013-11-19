6a6bddfb57a4527f49e1b35bb019133501386e88
romeoh78@gmail.com <romeoh78@baeg-ui-MacBook-Pro.local>
2013-11-19 16:53:16 +0900

    launch

--- js/applist.js
+++ js/applist.js
@@ -1,12 +1,13 @@
 var  appList = ''
-	,totalApp = 164
+	,totalApp = 165
 
-M('#btnTest').attr('href', 'http://goo.gl/jtH4H4');
-appList += '<li data-app="165" data-page="snow" data-new data-love><a href="http://goo.gl/jtH4H4">첫눈 오는날 나는 뭘하고 있을까?</a> <span class="ico new"></span></li>';
+M('#btnTest').attr('href', 'http://goo.gl/1NnHVG');
+appList += '<li data-app="166" data-page="idol" data-new data-fun><a href="http://goo.gl/1NnHVG">아이돌의 새멤버</a> <span class="ico new"></span></li>';
+appList += '<li data-app="165" data-page="snow" data-new data-hot data-love><a href="http://goo.gl/jtH4H4">첫눈 오는날 나는 뭘하고 있을까?</a> <span class="ico new"></span><span class="ico hot"></span></li>';
 appList += '<li data-app="164" data-page="will" data-new data-hot data-me><a href="http://goo.gl/JSLe1F">나는 앞으로 어떻게 변할까?</a> <span class="ico new"></span><span class="ico hot"></span></li>';
 appList += '<li data-app="163" data-page="more" data-new data-love><a href="http://goo.gl/CRtZwE">비타500 한병 더</a> <span class="ico new"></span></li>';
 appList += '<li data-app="162" data-page="goback" data-new data-love><a href="http://goo.gl/dOrlSz">나의 고백의 성공확률</a> <span class="ico new"></span></li>';
-appList += '<li data-app="161" data-page="skil" data-new data-love><a href="http://goo.gl/U5nKXJ">연애의 기술</a> <span class="ico new"></span></li>';
+appList += '<li data-app="161" data-page="skil" data-love><a href="http://goo.gl/U5nKXJ">연애의 기술</a></li>';
 appList += '<li data-app="160" data-page="jekuk" data-hot data-fun><a href="http://goo.gl/iPUfA4">나는 제국고에 전학갈수 있나?</a> <span class="ico hot"></span></li>';
 appList += '<li data-app="159" data-page="god" data-hot data-fun><a href="http://goo.gl/WwlvAa">나는 무슨 신(神)일까?</a> <span class="ico hot"></span></li>';
 appList += '<li data-app="158" data-page="truthgame" data-love><a href="http://goo.gl/n0bpBh">진실게임</a></li>';
