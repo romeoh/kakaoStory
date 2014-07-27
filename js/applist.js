47962485aa39feaa001a34636c26cd0ea5ccc20f
romeoh <romeoh78@gmail.com>
2014-07-27 14:20:08 +0900

    update

--- js/applist.js
+++ js/applist.js
@@ -1,15 +1,17 @@
 var  appList = ''
-	,totalApp = 203
+	,totalApp = 204
 
 M('#btnTest').attr('href', 'http://goo.gl/dfAPTw');
 //appList += '<li><a href="http://goo.gl/NEphmg">대~한민국★ <span style="font-weight:bold; color:#00C537">2004 브라질월드컵 일정</span></a> <span class="ico command"></span></li>';
 //appList += '<li><a href="http://goo.gl/xwrAKo">아이유의 생일이 궁금하다면? <br><span style="font-weight:bold; color:#00C537">모두의 디데이</span></a> <span class="ico command"></span></li>';
-appList += '<li><a href="http://goo.gl/GtCy5q"><img src="http://romeoh.github.io/gaeyouAppJs/common/img/ic.moa.game.png" style="width:40px; float:left; margin-right:10px"> 최신게임뉴스를 한눈에 <br><span style="font-weight:bold; color:#00C537">게임뉴스 모아보기</span></a> <span class="ico command"></span></li>';
+//appList += '<li><a href="http://goo.gl/GtCy5q"><img src="http://romeoh.github.io/gaeyouAppJs/common/img/ic.moa.game.png" style="width:40px; float:left; margin-right:10px"> 최신게임뉴스를 한눈에 <br><span style="font-weight:bold; color:#00C537">게임뉴스 모아보기</span></a> <span class="ico command"></span></li>';
+appList += '<li><a href="http://goo.gl/TSqLQW"><img src="http://romeoh.github.io/gaeyouAppJs/common/img/ic.arcade.png" style="width:40px; float:left; margin-right:10px"> 매일매일 업데이트되는 <br><span style="font-weight:bold; color:#00C537">오락실</span> 다운받기</a> <span class="ico command"></span></li>';
 
+appList += '<li data-app="205" data-page="enterfamily" data-new data-fun><a href="http://goo.gl/w3Wd0J">초성 노래제목 맞추기</a> <span class="ico new"></span></li>';
 appList += '<li data-app="204" data-page="enterfamily" data-new data-fun><a href="http://goo.gl/q6HtBL">나를 좋아하는 사람의 성은?</a> <span class="ico new"></span></li>';
 appList += '<li data-app="203" data-page="enterfamily" data-new data-fun><a href="http://goo.gl/dfAPTw">다음달에 애인이 생길 확률</a> <span class="ico new"></span></li>';
 appList += '<li data-app="202" data-page="enterfamily" data-new data-fun><a href="http://goo.gl/H19wSd">나의 천생연분의 초성</a> <span class="ico new"></span></li>';
-appList += '<li data-app="201" data-page="enterfamily" data-new data-hot data-fun><a href="http://goo.gl/ZxYUD7">연예인가족</a> <span class="ico new"></span><span class="ico hot"></span></li>';
+appList += '<li data-app="201" data-page="enterfamily" data-hot data-fun><a href="http://goo.gl/ZxYUD7">연예인가족</a> <span class="ico hot"></span></li>';
 appList += '<li data-app="200" data-page="star" data-new data-fun><a href="http://goo.gl/qHPzTV">별에서 온 그대</a> <span class="ico new"></span></li>';
 appList += '<li data-app="199" data-page="blood2014" data-fun><a href="http://goo.gl/b33j0A">별자리로 보는 2014년 나의운명</a> </li>';
 appList += '<li data-app="198" data-page="gaecon" data-hot data-fun><a href="http://goo.gl/cGx4dk">개그콘서트 캐릭터</a> <span class="ico hot"></span></li>';
