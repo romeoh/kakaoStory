af7dfa3e8938bc6ad0452e921000c8526dfb0820
romeoh <romeoh78@gmail.com>
2018-01-03 22:23:41 +0900

    list 수정

--- js/applist.js
+++ js/applist.js
@@ -7,7 +7,7 @@ M('#btnTest').attr('href', 'http://goo.gl/dfAPTw');
 //appList += '<li><a href="http://goo.gl/GtCy5q"><img src="http://romeoh.github.io/gaeyouAppJs/common/img/ic.moa.game.png" style="width:40px; float:left; margin-right:10px"> 최신게임뉴스를 한눈에 <br><span style="font-weight:bold; color:#00C537">게임뉴스 모아보기</span></a> <span class="ico command"></span></li>';
 //appList += '<li><a href="http://goo.gl/TSqLQW"><img src="http://romeoh.github.io/gaeyouAppJs/common/img/ic.arcade.png" style="width:40px; float:left; margin-right:10px"> 매일매일 업데이트되는 <br><span style="font-weight:bold; color:#00C537">오락실</span> 다운받기</a> <span class="ico command"></span></li>';
 //appList += '<li><a href="http://goo.gl/rbxiXH"><img src="http://romeoh.github.io/gaeyouAppJs/common/img/ic_ztalk.png" style="width:40px; float:left; margin-right:10px"> 골때리는 메신져 <br><span style="font-weight:bold; color:#00C537">ㅋ톡: 관심병을던져라</span></a> <span class="ico command"></span></li>';
-appList += '<li><a href="http://goo.gl/Nj2Zbe">친구들에게 나의 장점을 물어보세요.<span class="ico command"></span></li>';
+//appList += '<li><a href="http://goo.gl/Nj2Zbe">친구들에게 나의 장점을 물어보세요.<span class="ico command"></span></li>';
 
 appList += '<li data-app="213" data-page="aein" data-new data-fun><a href="http://goo.gl/QRYWAE">내 애인은 태어나기는 한 걸까?</a> <span class="ico new"></span></li>';
 appList += '<li data-app="212" data-page="insideout" data-new data-fun><a href="http://goo.gl/YgbqtY">인사이드아웃</a> <span class="ico new"></span></li>';
